2eced1a01c5ff91752952cb70c7ab4c96ea33e68
daniel <danielgold885@gmail.com>
2025-04-24 13:53:28 +0300

    wip

--- tests/test_form.py
+++ tests/test_form.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from configs.config import BASE_URL, FORM_PATH
 import requests
-from time import sleep
 from utils.color_utils import is_red_border
 from business_flows.site_validation import check_all_internal_links
 
